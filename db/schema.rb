b6bb1bcf7ad5140dae6e450059213b5eb3168096
zwang759 <legend313@outlook.com>
2020-07-26 20:50:11 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,39 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "question_tags", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text "question", null: false
+    t.string "created_by", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+  end
+
+  create_table "scoreboard", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
